b4d71e91e42bdb298264e59d006679bec69d19ab
Craig A Schotch II <caschotchii@gmail.com>
2017-04-06 19:00:20 -0700

    Added Keystones in JS Array with Graphics, Image-Class Folder created for better organization

---
+++ scripts/class.js
@@ -0,0 +1,97 @@
+var poeClass = [
+  {
+    class: "Witch",
+    ascendancy: "Necromancer",
+    images: "images/classes/witch-necromancer.png"
+  },
+  {
+    class: "Witch",
+    ascendancy: "Occultist",
+    images: "images/classes/witch-occultist.png"
+  },
+  {
+    class: "Witch",
+    ascendancy: "Elementalist",
+    images: "images/classes/witch-elementalist.png"
+  },
+  {
+    class: "Duelist",
+    ascendancy: "Champion",
+    images: "images/classes/duel-champion.png"
+  },
+  {
+    class: "Duelist",
+    ascendancy: "Gladiator",
+    images: "images/classes/duel-gladiator.png"
+  },
+  {
+    class: "Duelist",
+    ascendancy: "Slayer",
+    images: "images/classes/duel-slayer.png"
+  },
+  {
+    class: "Marauder",
+    ascendancy: "Berserker",
+    images: "images/classes/mar-berserker.png"
+  },
+  {
+    class: "Marauder",
+    ascendancy: "Chieftain",
+    images: "images/classes/mar-chieftain.png"
+  },
+  {
+    class: "Marauder",
+    ascendancy: "Juggernaut",
+    images: "images/classes/mar-juggernaut.png"
+  },
+  {
+    class: "Ranger",
+    ascendancy: "Deadeye",
+    images: "images/classes/ranger-deadeye.png"
+  },
+  {
+    class: "Ranger",
+    ascendancy: "Pathfinder",
+    images: "images/classes/ranger-pathfinder.png"
+  },
+  {
+    class: "Ranger",
+    ascendancy: "Raider",
+    images: "images/classes/ranger-raider.png"
+  },
+  {
+    class: "Shadow",
+    ascendancy: "Assassin",
+    images: "images/classes/shad-assasin.png"
+  },
+  {
+    class: "Shadow",
+    ascendancy: "Saboteur",
+    images: "images/classes/shadow-saboteur.png"
+  },
+  {
+    class: "Shadow",
+    ascendancy: "Trickster",
+    images: "images/classes/shadow-trickster.png"
+  },
+  {
+    class: "Templar",
+    ascendancy: "Guardian",
+    images: "images/classes/temp-guardian.png"
+  },
+  {
+    class: "Templar",
+    ascendancy: "Hierophant",
+    images: "images/classes/temp-hierophant.png"
+  },
+  {
+    class: "Templar",
+    ascendancy: "Inquisitor",
+    images: "images/classes/temp-inquisitor.png"
+  },
+  {
+    class: "Scion",
+    ascendancy: "Ascendant",
+    images: "images/classes/scion-ascendant.png"
+  }
+];
